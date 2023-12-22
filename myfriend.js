033793ccdd051c2cf6d2382b476e6119f52d23b7
AssassinOFsilchar <abhiran14082002@gmail.com>
2023-12-22 14:42:41 +0530

    thats how u do it

---
+++ myfriend.js
@@ -0,0 +1,41 @@
+const populate = async (value, currency) => {
+    let myStr = ""
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_eEfK2pmR6rorSWnJpukEfhsoU3LyPMKRIZv5sosW&base_currency=" + currency
+    let response = await fetch(url)
+    let rJson = await response.json()
+    document.querySelector(".output").style.display = "block"
+
+    for (let key of Object.keys(rJson["data"])) {
+        myStr += ` <tr>
+                        <td>${key}</td>
+                        <td>${rJson["data"][key]["code"]}</td>
+                        <td>${(rJson["data"][key]["value"] * value)}</td>
+                    </tr> 
+                `
+    }
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = myStr;
+
+}
+const btn = document.querySelector(".btn")
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    const value = parseInt(document.querySelector("input[id='quantity']").value);
+    const currency = document.querySelector("select[id='currency']").value
+    populate(value, currency)
+})
+
+
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
